73c23553b4d2072a5f7139e34ce32fbf52e8c824
Ahmad Mazaal <Ahmad.hmazaal@gmail.com>
2023-03-29 04:33:19 +0100

    feat: add AWS secret manager

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,3 +1,33 @@
+import * as dotenv from 'dotenv';
+const AWS = require('aws-sdk');
+
+export const loadSecrets = async () => {
+    const secretName = "book-trader-backend";
+    dotenv.config();
+    const input = {
+        region: process.env.AWS_REGION,
+        accessKeyId: process.env.AWS_KEY_ID,
+        secretAccessKey: process.env.AWS_ACCESS_SECRET,
+    }
+    const client = new AWS.SecretsManager(input);
+    try {
+        let secrets;
+        const data = await client.getSecretValue({ SecretId: secretName }).promise();
+        if ('SecretString' in data) {
+            secrets = JSON.parse(data.SecretString);
+        } else {
+            const decodedBinarySecret = Buffer.from(data.SecretBinary, 'base64');
+            secrets = decodedBinarySecret;
+        }
+        for (const [key, value] of Object.entries(secrets)) {
+            (process.env[key] as any) = value;
+        }
+    } catch (error) {
+        //? For a list of exceptions thrown, see:  https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        throw error;
+    }
+}
+
 const configuration = () => {
     return {
         port: process.env.PORT || 3000,
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { loadSecrets } from './config/configuration';
 
 async function bootstrap() {
+    await loadSecrets();
     const app = await NestFactory.create(AppModule);
     await app.listen(3000).then(
         () => console.log('Backend up and running on port 3000'.toUpperCase())
